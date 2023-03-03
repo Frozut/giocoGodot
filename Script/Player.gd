d0ed6b7e95bf3c493d6dc47776fd595f108765a3
Delfa <85013277+Frozut@users.noreply.github.com>
2023-03-03 21:03:07 +0100

    player modify

--- Script/Player.gd
+++ Script/Player.gd
@@ -16,13 +16,9 @@ func _physics_process(delta):
 	
 	if input.x == 0:
 		applay_friction()
+	else :
+		applay_acceleration(input.x)
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50;
-	else:
-		velocity.x = 0
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y=-100
 		
@@ -32,4 +28,7 @@ func apply_gvavity():
 	velocity.y += GRAVITY
 	
 func applay_friction():
-	pass
+	velocity.x = move_toward(velocity.x,0, 2)
+	
+func applay_acceleration(amount):
+	velocity.x = move_toward(velocity.x,50 * amount, 2)
