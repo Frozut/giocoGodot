c66a50bf187dd2206e7269254815e175656a82f1
Delfa <85013277+Frozut@users.noreply.github.com>
2023-03-08 19:17:38 +0100

    commenti

--- Script/hitbox.gd
+++ Script/hitbox.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+#abbiamo creato questa classe hitbox per potere rendere universale il collision shape dei nemici
+#grazie a cio noi possiamo andare sui nodi che ci interessano abbiano una hitbox contro il nostro player e cliccare collega
+#o ctrl shift a 
 
 func _on_body_entered(body):
 	if body is Player:
