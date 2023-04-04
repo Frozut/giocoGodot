6f2fafbbf048beeb70ec46d8ca2e75cb69f975b9
Delfa <85013277+Frozut@users.noreply.github.com>
2023-04-04 10:00:51 +0200

    fine implementazione checkpoint e morte del personaggio

---
+++ Script/check_point.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _on_body_entered(body):
+	if not body is Player : return
+	animated_sprite.play("checked") 
+	Events.emit_signal("hit_checkpoint",position)
--- Script/world.gd
+++ Script/world.gd
@@ -4,7 +4,7 @@ const  playerScene = preload("res://Player and Enemy/player.tscn")
 
 var player_spaw_location = Vector2.ZERO
 
-@onready var Player: = $Player
+@onready var player: = $Player
 @onready var Camera = $Camera2D
 @onready var timer = $Timer
 
@@ -13,24 +13,30 @@ func _ready():
 	#va a riempire i colori vuoti con il colore che gli diciamo noi, in questo caso ci servira per lo sfondo
 	RenderingServer.set_default_clear_color(Color.LIGHT_BLUE)
 	#andiamo a settera la camera sul personaggio tramite codice con il metodo connect_camera
-	Player.connect_camera(Camera) 
+	player.connect_camera(Camera) 
 	#grazie alla funzione sottostante riusciamo a prendere la posiizone del palyer quando spowna per successivamente settarla quando creamoun nuovo player
-	player_spaw_location = Player.global_position
+	player_spaw_location = player.global_position
 	#questo serve per collegare l'evento che abbiamo creato nella script Events() che adesso e` una variabile globale 
 	#guarda Soud player per capire come si fa, ATTENZIONE E` DIVERSO RISPETO Alla precedente versione di godot
 	Events.player_died.connect(_on_player_died)
+	Events.hit_checkpoint.connect(_on_hit_checkpoint)
 
 
 #questa funzione si collega con quella l'events che ce` sopra
 func _on_player_died():
 	#impostiamo il timer a 1 secondo 
 	timer.start(1.0)
-	#prima di afre quello che segue la riga sottostante aspettimao che il timer finisca grazie alla funzioe await
+	#prima di fare quello che segue la riga sottostante aspettimao che il timer finisca grazie alla funzioe await
 	await timer.timeout
 	#creaiamo il player nuovo  con questa riga di codice 
 	var player = playerScene.instantiate()
+	#settimano la posizion del player che abbiamo preso sopra quando abbiamo creato il mondo con Player.global_position
+	player.global_position= player_spaw_location
 	#successivamente lo aggiungiamo alla mappa
 	add_child(player)
-	player.global_position= player_spaw_location
 	#ed infine lo collegghiamo alla camera
 	player.connect_camera(Camera)
+
+func _on_hit_checkpoint(checkpoint_position):
+	player_spaw_location = checkpoint_position
+	
