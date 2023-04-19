248e98d678f58dfe3778fbc8540e75e6b171df31
Delfa <andreadfabbro@hotmail.it>
2023-04-19 13:56:04 +0200

    implementazione menu prova e coin

---
+++ Script/Menu/menuProva.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_start_button_pressed() -> void:
+	Transition.play_exit_transition()
+	#grazie a questo comando mettimano il gioco in pausa
+	get_tree().paused = true 
+	#con questo comanto aspettimao finche il segnale che abbimao creato venga emesso 
+	await Transition.animation_completed
+	#faccimao partire l'aniamzione di entrata
+	Transition.play_enter_transition()
+	#togliamo il gioco dalla pausa
+	get_tree().paused = false 
+	get_tree().change_scene_to_file("res://Livelli/TrueLevels/Level1.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
--- Script/coin.gd
+++ Script/coin.gd
@@ -5,3 +5,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body is Player:return
+	var tween = create_tween()
+	tween.tween_property(self,"position",position+Vector2(0,-30),0.5)
+	tween.tween_property(self,"modulate:a",0.0,0.5)
+	tween.tween_callback(self.queue_free)
+	
